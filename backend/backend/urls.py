decf7ace2067628afbb46f47aca3bf07dfad0bdd
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-09-04 11:46:51 +0200

    bugfixes + migrations

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from portfolio.views import PorfolioItemViewSet
+from portfolio.views import PortfolioItemViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 
+router = routers.DefaultRouter()
+router.register(r"portfolio", PortfolioItemViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(routers.urls))
+    path('api/', include(router.urls))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from .serializers import PortfolioItemSerializer
 from .models import PortfolioItem
 
-class PorfolioItemViewSet(viewsets.ModelViewSet):
+class PortfolioItemViewSet(viewsets.ModelViewSet):
     queryset = PortfolioItem.objects.all().order_by("created_at")
     serializer_class = PortfolioItemSerializer
