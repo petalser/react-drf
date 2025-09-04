e379b4d90bfef13b6dcf0b4c561ded5d192aeb98
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-09-04 11:21:53 +0200

    db schema template

--- backend/portfolio/models.py
+++ backend/portfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class PortfolioItem(models.Model):
+    title = models.CharField(max_length=200)
+    #TODO: file validation
+    before_image = models.ImageField(upload_to="before/")
+    #TODO: file validation
+    after_image = models.ImageField(upload_to="after/")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
